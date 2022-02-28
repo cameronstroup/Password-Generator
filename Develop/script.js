27ddb7647b54d2e606bc7e15ef4ca85ef57504fb
cameronstroup <90347622+cameronstroup@users.noreply.github.com>
2022-02-27 19:55:33 -0600

    finsished project

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,3 @@
-// Assignment code here
-
 function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 96);
 }
@@ -21,6 +19,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function userOptions() {
+  var count = 0;
   var confirmLength = parseInt(
     prompt("How long would you like your password? (Must be between 8-128)")
   );
@@ -33,13 +32,33 @@ function userOptions() {
   var confirmL = confirm("Would you like to use lowercase letters?");
   var confirmNum = confirm("Would you like to use number letters?");
   var confirmS = confirm("Would you like to use special characters?");
+
+  // Im using the count to know what to divide the loop length with
+
+  if (confirmU === true) {
+    count++;
+  }
+  if (confirmL === true) {
+    count++;
+  }
+  if (confirmS === true) {
+    count++;
+  }
+  if (confirmNum === true) {
+    count++;
+  }
+
   var confirmations = {
     confirmLength: confirmLength,
     confirmU: confirmU,
     confirmNum: confirmNum,
     confirmS: confirmS,
     confirmL: confirmL,
+    count: count,
   };
+
+  console.log(confirmations);
+
   return confirmations;
 }
 function generatePassword() {
@@ -47,7 +66,7 @@ function generatePassword() {
   console.log(options);
   var randomChar = [];
 
-  for (i = 0; i < options.confirmLength / 4; i++) {
+  for (i = 0; i < options.confirmLength / options.count; i++) {
     if (options.confirmU === true) {
       randomChar.push(getRandomUper());
     }
